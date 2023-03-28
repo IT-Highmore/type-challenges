e5825f3866f8c7a82476a351af4f36be300b6a35
andrew jarrett <15133992+ahrjarrett@users.noreply.github.com>
2023-03-28 09:54:19 +0000

    feat(question): add #25747 - IsNegativeNumber

---
+++ questions/25747-hard-isnegativenumber/test-cases.ts
@@ -0,0 +1,20 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type AssertAll<T extends ReadonlyArray<true>> = T
+type Not<A extends boolean> = false extends A ? true : false
+type IsNever<A> = [A] extends [never] ? true : false
+
+type cases = [
+  AssertAll<[
+    Not<IsNegativeNumber<0>>,
+    IsNever<IsNegativeNumber<number>>,
+    IsNever<IsNegativeNumber<-1 | -2>>,
+    IsNegativeNumber<-1>,
+    IsNegativeNumber<-1.9>,
+    IsNegativeNumber<-100_000_000>,
+    Not<IsNegativeNumber<1>>,
+    Not<IsNegativeNumber<1.9>>,
+    Not<IsNegativeNumber<100_000_000>>,
+  >
+]>
